4480efc47558f0b4db11fe617da79d2f65434cf3
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-21 09:45:51 -0700

    initial commit with some refactors done

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
+});
+
+await server.start();
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
