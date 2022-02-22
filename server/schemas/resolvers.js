4c2c90a1624cf4720e27eb7914de506431f3b8eb
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-22 16:40:00 -0700

    login resolver works

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,6 +21,22 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    login: async (parent, { email, password }, context) => {
+      const user = await User.findOne({
+        $or: [{ username: email }, { email: email }],
+      });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new Error("Wrong password!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
   },
 };
 
